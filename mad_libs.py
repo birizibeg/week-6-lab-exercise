c717663fccbfef27fa456afbc97e90196170d3d7
zhy75 <zhy75@jupyterhub.crc.pitt.edu>
2019-10-02 14:47:03 -0400

    Created python program

---
+++ mad_libs.py
@@ -0,0 +1,21 @@
+name1 = input( "Enter your name:" )
+name2 = input( "Enter another name:" )
+name3 = input( "Enter another name:")
+number = input( "Enter a number:" )
+animal = input( "Enter an animal:")
+verb = input( "Enter a verb:" )
+adjective1 = input( "Enter an adjective that describes a day:")
+number2 = input( "Enter another number" )
+adjective2 = input( "Enter an adjective that describes newly found frienship:")
+
+print( "\n\n\n\nIt was a rather", adjective1, "day.", name1, "and", name2,
+     "were taking a Computer Science class at Pitt.", name3, 
+      "was the instructor of this lab, and he ruthlessly assigned",
+      number, "exercises for homework. One of them was to",
+      verb, "a(n)", animal, "to sleep. After", number2, "minutes, ",
+      name1, "was desperate beacuse he doesn't know how to start.", 
+      "But right at this very moment,", name2, "appeared out of no",
+      "where and asked to team up with", name1, ". Fate has joined these",
+      "two strangers together, and at it blossomed into a(n)",
+      adjective2, "frienship." )
+
